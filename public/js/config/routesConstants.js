9a778e938f532da7aacb9f1ed9f22a07ff63361e
nvjot94 <nvjot94@gmail.com>
2018-02-25 18:49:17 +0530

    Update routesConstants.js

--- public/js/config/routesConstants.js
+++ public/js/config/routesConstants.js
@@ -1,4 +1,8 @@
-app.constant('HOME','/');
-app.constant('PRODUCTS','/products:');
-app.constant('CHOOSEPRODUCT','/chooseproduct');
-app.constant('CATEGORIES','/categories/:categoryname');
+app.constant('HOME', '/');
+app.constant('PRODUCTS', '/products/:subcategory');
+app.constant('CHOOSEPRODUCT', '/chooseproduct/:productId');
+app.constant('CATEGORIES', '/categories/:categoryname');
+app.constant('WISHLIST', '/wishlist');
+app.constant('CART', '/cart');
+app.constant('CHECKOUT', '/checkout');
+app.constant('MYORDERS', '/myorders');
