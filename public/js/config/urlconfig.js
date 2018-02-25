806b789bc146b34850b612f1b0ec0026e6e610a8
nvjot94 <nvjot94@gmail.com>
2018-02-25 18:25:36 +0530

    Update urlconfig.js

--- public/js/config/urlconfig.js
+++ public/js/config/urlconfig.js
@@ -1,3 +1,3 @@
-app.constant("DATAURL", "http://localhost:3000/json/fake.json");
+app.constant("DATAURL", "https://raw.githubusercontent.com/nvjot94/jsonData/master/fake.json");
 app.constant("MENUURL", "http://localhost:3000/json/menu.json");
 app.constant("PRODUCTURL", "http://localhost:3000/json/products.json");
